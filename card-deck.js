6d4ff4f45e996add2894fbbe98c42ef835880f2b
Joseph Perry <josephperry720@gmail.com>
2021-02-03 09:33:01 -0500

    added query string for hand size

--- card-deck.js
+++ card-deck.js
@@ -1,23 +1,54 @@
 exports.handler = async (event) => {
-    const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-    const pip = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
-    const hand = [];
-    let card = "";
-    while (hand.length < 5) {
-        let randomSuit = suits[Math.floor(Math.random() * suits.length)];
-        let randomPip = pip[Math.floor(Math.random() * pip.length)];
-        card = `${randomPip} of ${randomSuit}`;
-        if (!hand.includes(card)) {
-            hand.push(card);
-        }
-    }
-    const cardHand = {
-        asString: `Your cards: ${hand.join(", ")}`,
-        asArray: hand
-    }
-        const response = {
-        statusCode: 200,
-        body: JSON.stringify(cardHand),
+  const { hand_size } = event.queryStringParameters;
+  const handSize = parseInt(hand_size);
+  if (handSize > 52 || handSize < 1) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        success: false,
+        hand: null,
+        message: "Invalid hand size. Must be between one 1 and 52",
+      }),
     };
     return response;
+  }
+  const SUITS = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  const PIP = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+  ];
+  const hand = [];
+  let card = "";
+  while (hand.length < handSize) {
+    let randomSuit = SUITS[Math.floor(Math.random() * SUITS.length)];
+    let randomPip = PIP[Math.floor(Math.random() * PIP.length)];
+    card = `${randomPip} of ${randomSuit}`;
+    if (!hand.includes(card)) {
+      hand.push(card);
+    }
+  }
+  const cardHand = {
+    asString: `Your cards: ${hand.join(", ")}`,
+    asArray: hand,
+  };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+        success: true,
+        hand: cardHand,
+        message: `${hand_size} cards delt`,
+    }),
+  };
+  return response;
 };
